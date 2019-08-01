772df86a3f34226c8da3e54da64f3af7109d60b7
aeksei <aeksei94@gmail.com>
2019-08-01 11:39:07 +0300

    dataframe from parse line

--- parser_product.py
+++ parser_product.py
@@ -2,17 +2,21 @@ import pandas as pd
 
 
 def parse_shop(line='1k мелочей: солнечные круги - 1, битые пиксели - 10'):
-    shop = line[:line.find(':')]
+    product = {'shop': line[:line.find(':')]}
     description = line[line.find(':') + 1:].strip()
-    product = {}
     for d in description.split(','):
         position = d.strip().split(' - ')
         product[position[0]] = float(position[1])
-    return shop, product
+    return product
 
 
 FILE = 'input.txt'
-
+df = pd.DataFrame()
 with open(FILE, 'r') as f:
     for line in f:
-        print(parse_shop(line))
+        product = parse_shop(line)
+        df = df.append(product, ignore_index=True)
+df = df.set_index('shop')
+
+print(df)
+
